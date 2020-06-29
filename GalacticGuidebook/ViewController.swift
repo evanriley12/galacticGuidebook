0181c4df304095c3da225fa43399312d3a33edb0
Evan Riley <evanriley@unomaha.edu>
2020-06-29 10:12:47 -0500

    Initial Commit

---
+++ GalacticGuidebook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GalacticGuidebook
+//
+//  Created by Evan Riley on 6/29/20.
+//  Copyright © 2020 Evan Riley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
